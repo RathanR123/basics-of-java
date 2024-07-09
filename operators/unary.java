5684b0b3286586b5600964f149352b4cb7bc7e18
RathanR123 <137428872+RathanR123@users.noreply.github.com>
2024-07-09 12:27:03 +0530

    Add files via upload

---
+++ operators/unary.java
@@ -0,0 +1,12 @@
+class unary 
+{
+    public static void main(String args[])
+    {
+        int num = 7;
+        num++;
+        num--;
+        --num;
+        ++num;
+        System.out.println(num);
+    }
+}
